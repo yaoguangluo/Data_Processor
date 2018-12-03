46a55c35e07f0d918aa30151a0ebf392c61ab4ca
罗瑶光 <lyg.tin@gmail.com>
2018-12-04 02:08:27 +0800

    Update Quick_6D_luoyaoguang_Sort.java

--- sortProcessor/Quick_6D_luoyaoguang_Sort.java
+++ sortProcessor/Quick_6D_luoyaoguang_Sort.java
@@ -16,7 +16,7 @@ public class Quick_6D_luoyaoguang_Sort{
 	private void partition(int[] a, int lp, int rp, int[]pos) {
 		// TODO Auto-generated method stub
 		int x,lp1,rp1,temp;x=a[lp];rp1=rp;lp1=lp;int y=a[rp];
-		if(x<y){while(lp1<rp1)
+		if(x<=y){while(lp1<rp1)
 		{while((a[lp1]<=x)&&(lp1<rp1)) lp1++;while(a[rp1]>x)rp1--;
 		if(lp1<rp1){temp=a[rp1];a[rp1]=a[lp1];a[lp1]=temp;}}
 		a[lp]=a[rp1];a[rp1]=x;}else{
