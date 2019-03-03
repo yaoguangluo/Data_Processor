fba8fd4d835ccc377798b20f9da2700850a1389e
yaoguangluo <fengyue1985>
2019-03-04 03:47:09 +0800

    隐写前处理函数集合。/ steganographyProcessor function collections.

--- DP/steganographyProcessor/PixFloat.java
+++ DP/steganographyProcessor/PixFloat.java
@@ -19,4 +19,29 @@ public class PixFloat{
 			}
 		}
 	}
+
+	public int[] matrixToLiner(int[][] pix) {
+		int[] output = new int[pix.length * pix[0].length];
+		for(int x = 0; x < pix[0].length; x ++) {
+			for(int y = 0; y < pix.length; y ++) {
+				output[x * pix[0].length + y] = pix[x][y];
+			}
+		}
+		return output;
+	}
+
+	public String LinerToLineCode(int[] pix) {
+		StringBuilder code = new StringBuilder();
+		for(int i = 0; i < pix.length; i++){
+			String register = String.valueOf(pix[i]);
+			code.append("" + register.charAt(0));
+			if(register.length()>1) {
+				code.append("" + register.charAt(1));
+			}
+			if(register.length()>2) {
+				code.append("" + register.charAt(2));
+			}
+		}
+		return code.toString();
+	}
 }
