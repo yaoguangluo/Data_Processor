516cc96ad3cc1fc44fb4722431339216ff9f58b5
Lenovo <Lenovo@LAPTOP-352KOFCM>
2019-03-17 08:18:49 +0800

    luo‘s quick 6d optimization

--- DP/sortProcessor/Quick_6D_luoyaoguang_Sort.java
+++ DP/sortProcessor/Quick_6D_luoyaoguang_Sort.java
@@ -8,8 +8,8 @@ public class Quick_6D_luoyaoguang_Sort{
 
 	private void quick2ds(int[] a, int lp, int rp) {
 		if(lp < rp){
-			int c = rp - lp + 1; if(c < 7){ int j;
-			for(int i = 1 + lp; i < lp + c; i++){
+			int c = rp - lp; if(c < 7){ int j;
+			for(int i = 1 + lp; i <= lp + c; i++){
 				j = i;while(j>=1+lp){
 					if(a[j]<a[j-1]){
 						int temp=a[j];a[j]=a[j-1];a[j-1]=temp;
