a64d57a37d15f940eafaeb3a2abdbbe326bb65d8
yaoguangluo <fengyue1985>
2019-03-28 04:06:50 +0800

    quick sort with liner table mode

--- DP/sortProcessor/Quick_6D_luoyaoguang_Sort.java
+++ DP/sortProcessor/Quick_6D_luoyaoguang_Sort.java
@@ -45,4 +45,51 @@ public class Quick_6D_luoyaoguang_Sort{
 		a[lp]=a[rp1];a[rp1]=x;
 		return rp1;
 	}
+	
+	public String[][] sort(String[][] a) {
+		quick2dsString(a, 0, a.length-1);
+		return a;
+	}
+
+	private void quick2dsString(String[][] a, int lp, int rp) {
+		if(lp < rp){
+			int c = rp - lp; if(c < 7){ int j;
+			for(int i = 1 + lp; i <= lp + c; i++){
+				j = i;while(j>=1+lp){
+					if(Double.valueOf(a[j][1])<Double.valueOf(a[j-1][1])){
+						String []temp=a[j];
+						a[j]=a[j-1];
+						a[j-1]=temp;
+					}
+					j--;
+				}
+			}	
+			return;
+			}
+			int pos = partitionString(a, lp, rp);
+			quick2dsString(a,lp,pos-1);
+			quick2dsString(a,pos+1,rp);
+		}
+	}
+
+	private int partitionString(String[][] a, int lp, int rp) {
+		String[] x=a[lp];int rp1=rp;int lp1=lp;
+		if(Double.valueOf(x[1])>=Double.valueOf(a[rp][1])){
+			x=a[rp];
+		}
+		while(lp1<rp1){
+			while((Double.valueOf(a[lp1][1])<=Double.valueOf(x[1]))&&(lp1<rp1)){
+				lp1++;
+			} 
+			while(Double.valueOf(a[rp1][1])>Double.valueOf(x[1])){
+				rp1--;
+			}
+			if(lp1<rp1){
+				String[] temp=a[rp1];
+				a[rp1]=a[lp1];a[lp1]=temp;
+			}
+		}
+		a[lp]=a[rp1];a[rp1]=x;
+		return rp1;
+	}
 }
