b6ec5ae64c78e1d054b5705dc50c80fc750996ce
yaoguangluo <fengyue1985>
2019-04-12 10:36:24 +0800

    swap 修复 进行了修正.

--- DP/DNAProcessor/SwapLaw.java
+++ DP/DNAProcessor/SwapLaw.java
@@ -1,20 +1,9 @@
 package DNAProcessor;
-
-import java.util.Iterator;
-import java.util.concurrent.ConcurrentLinkedDeque;
-
 public class SwapLaw{
-	public ConcurrentLinkedDeque<DNA> swap(ConcurrentLinkedDeque<DNA> original) {
-		ConcurrentLinkedDeque<DNA> output= new ConcurrentLinkedDeque<>();
-		//random
-		Iterator<DNA> iterator= original.iterator();
-		while(iterator.hasNext()) {
-			DNA dna= iterator.next();
-			dna.setFunctionLineDeque(new CopyLaw().copy(dna.getMaskLineDeque()));	
-			dna.setFunctionLineMap(new CopyLaw().copyMap(dna.maskLineDeque));	
-			dna.setMaskLineMap(new CopyLaw().copyMap(dna.functionLineDeque));	
-			output.add(dna);
-		}
-		return output;
+	public DNA swap(DNA original) {	
+		original.setFunctionLineDeque(new CopyLaw().copy(original.getMaskLineDeque()));	
+		original.setFunctionLineMap(new CopyLaw().copyMap(original.maskLineDeque));	
+		original.setMaskLineMap(new CopyLaw().copyMap(original.functionLineDeque));	
+		return original;
 	}
 }
